ebaa4f0e2320f2de4111f3b6eff6180fd9a6cdaa
Josh Bedo <joshbedo@gmail.com>
2022-04-27 02:47:33 -0400

    remove unused function call

--- site/components/common/TopNav/TopNav.tsx
+++ site/components/common/TopNav/TopNav.tsx
@@ -262,7 +262,7 @@ const Nav: FC<NavProps> = ({ links }) => {
           <button
             type="button"
             className="bg-white p-2 rounded-md text-gray-400 lg:hidden"
-            onClick={() => setOpen(true)}
+            // onClick={() => setOpen(true)}
           >
             <span className="sr-only">Open menu</span>
             <MenuIcon className="h-6 w-6" aria-hidden="true" />
